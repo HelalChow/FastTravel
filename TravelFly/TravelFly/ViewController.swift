0a28d07da34d4ad5adf36f085128dedfb03922e2
hchowdhury3273 <hc2324@nyu.edu>
2019-08-28 21:33:14 -0400

    speech recognition

--- TravelFly/TravelFly/ViewController.swift
+++ TravelFly/TravelFly/ViewController.swift
@@ -9,15 +9,82 @@
 import UIKit
 import FlyoverKit
 import MapKit
+import Speech
 
-class ViewController: UIViewController, MKMapViewDelegate {
+class ViewController: UIViewController, MKMapViewDelegate, SFSpeechRecognizerDelegate {
+    
+    var userInputLocation = FlyoverAwesomePlace.parisEiffelTower
+    let speechRecognizer: SFSpeechRecognizer? = SFSpeechRecognizer(locale: Locale.init(identifier: "en-us"))
+    var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    var recognitionTask: SFSpeechRecognitionTask?
+    let audioEngine = AVAudioEngine()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        speechRecognizer?.delegate = self
+        SFSpeechRecognizer.requestAuthorization {
+            status in
+            var buttonState = false
+            switch status {
+            case .authorized:
+                buttonState = true
+                print("Permission recieved")
+            case .denied:
+                buttonState = false
+                print("User did not grant permssion for speech recognition")
+            case .notDetermined:
+                buttonState = false
+                print("Speech recofgnition not allowed by user")
+            case .restricted:
+                buttonState = false
+                print("Speech recognition is not supported on this device")
+                
+            }
+        }
+        
         self.mapSetup()
         
         
+    }
+    
+
+  
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var locationButton: UIButton!
+    @IBOutlet weak var placeLabel: UILabel!
+    
+    
+    
+    @IBAction func locationButtonClicked(_ sender: Any) {
+        
+        
+        
+        
+        
+        
+        
+//        let rand = locationDictionary.randomElement()!
+//        let camera = FlyoverCamera(mapView: self.mapView, configuration: FlyoverCamera.Configuration(duration: 6.0, altitude: 300, pitch: 45.0, headingStep: 40.0))
+//        camera.start(flyover: rand.value)
+//        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(6), execute: {
+//            camera.stop()
+//        } )
+        
+//        placeLabel.text = "\(rand.key)"
+    }
+    
+    func mapSetup() {
+        self.mapView.mapType = .hybridFlyover
+        self.mapView.showsBuildings = true
+        self.mapView.isZoomEnabled = true
+        self.mapView.isScrollEnabled = true
+        
+        let camera = FlyoverCamera(mapView: self.mapView, configuration: FlyoverCamera.Configuration(duration: 56.0, altitude: 300, pitch: 45.0, headingStep: 20.0))
+        camera.start(flyover: self.userInputLocation)
+        DispatchQueue.main.asyncAfter(deadline: .now() + .second(9), execute: {
+            camera.stop()
+        })
     }
     
     let locationDictionary = [
@@ -30,31 +97,7 @@ class ViewController: UIViewController, MKMapViewDelegate {
         "London Eye": FlyoverAwesomePlace.londonEye,
         "Eiffel Tower": FlyoverAwesomePlace.parisEiffelTower
     ]
-    
-  
-    @IBOutlet weak var mapView: MKMapView!
-    @IBOutlet weak var locationButton: UIButton!
-    @IBOutlet weak var placeLabel: UILabel!
-    
-    
-    
-    @IBAction func locationButtonClicked(_ sender: Any) {
-        let rand = locationDictionary.randomElement()!
-        let camera = FlyoverCamera(mapView: self.mapView, configuration: FlyoverCamera.Configuration(duration: 6.0, altitude: 300, pitch: 45.0, headingStep: 40.0))
-        camera.start(flyover: rand.value)
-        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(6), execute: {
-            camera.stop()
-        } )
-        
-        placeLabel.text = "\(rand.key)"
-    }
-    
-    func mapSetup() {
-        self.mapView.mapType = .hybridFlyover
-        self.mapView.showsBuildings = true
-        self.mapView.isZoomEnabled = true
-        self.mapView.isScrollEnabled = true
-    }
+
     
 }
 
